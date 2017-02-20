c86ee8f8cfa405987cddf72533be6c511353a440
ertex <davidlgjohansson@gmail.com>
2017-02-20 10:19:52 +0100

    dont_want_tooo

--- src/shit/pather/PathObject.java
+++ src/shit/pather/PathObject.java
@@ -1,7 +1,21 @@
 package shit.pather;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class PathObject {
 // PathObject and shit.pather belongs to David Johansson Te2
+private int x,y,width,height;
+private boolean foundExit,acive;
+private Color color;
+private byte type,lenght;
 
 
 
+public void CheckSurroundings(){
+
+}
+public void draw(Graphics g){
+
+}
 }
--- src/shit/pather/PathProgram.java
+++ src/shit/pather/PathProgram.java
@@ -1,7 +1,22 @@
 package shit.pather;
+
+import java.awt.Graphics;
+
 public class PathProgram {
 // PathProgram and shit.pather belongs to David Johansson Te2
 
+    public PathProgram(){
+    CreateAndShowGui();
+    }
+    
+public void CreateAndShowGui(){
 
+}
 
+public void draw(Graphics g){
+
+}
+public void run(){
+
+}
 }
