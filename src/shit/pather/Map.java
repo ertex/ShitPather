36e3ebe9f5c5758b7f718aee0e04705113f69a13
ertex <davidlgjohansson@gmail.com>
2017-02-20 10:13:41 +0100

    fixed_Map_methods

--- src/shit/pather/Map.java
+++ src/shit/pather/Map.java
@@ -1,7 +1,33 @@
 package shit.pather;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+
 public class Map {
 // Map and shit.pather belongs to David Johansson Te2
+private byte map[][][];
+private ArrayList<PathObject> Pathers;
+private int x,y,width,height;
+private int PathObjectSize;
+private PathObject OriginPathObject;
 
+public Map(){
 
+}
 
+public void draw(Graphics g){
+
+}
+public void updatePathing(){
+
+}
+public void changeMap(byte x,byte y,byte type){
+
+}
+public void resetMap(){
+
+}
+public void findExit(PathObject origin,PathObject exit){
+
+}
 }
