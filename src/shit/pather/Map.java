9ad1294ca1994f55b42ccf36c72827c44cf6e988
ertex <davidlgjohansson@gmail.com>
2017-02-20 10:42:27 +0100

    actionhandlerAndwhatnot

--- src/shit/pather/Map.java
+++ src/shit/pather/Map.java
@@ -5,14 +5,15 @@ import java.util.ArrayList;
 
 public class Map {
 // Map and shit.pather belongs to David Johansson Te2
-private byte map[][][];
+private byte map[][][],mapSize;
 private ArrayList<PathObject> Pathers;
 private int x,y,width,height;
-private int PathObjectSize;
+private final int PathObjectSize = 20;
 private PathObject OriginPathObject;
 
 public Map(){
-
+mapSize = 20;
+map = new byte[mapSize][mapSize][mapSize];
 }
 
 public void draw(Graphics g){
@@ -29,5 +30,11 @@ public void resetMap(){
 }
 public void findExit(PathObject origin,PathObject exit){
 
+}
+public int getPathObjectSize(){
+return PathObjectSize;
+}
+public byte getMapSize(){
+return mapSize;
 }
 }
--- src/shit/pather/PathProgram.java
+++ src/shit/pather/PathProgram.java
@@ -1,22 +1,79 @@
 package shit.pather;
 
+import java.awt.Canvas;
 import java.awt.Graphics;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 
 public class PathProgram {
+
+    Canvas canvas;
+    private int xSize, ySize;
+    Map map;
+    private JButton button;
+    private JPanel guiPanel;
+    private ActionHandler actionHandler = new ActionHandler();
 // PathProgram and shit.pather belongs to David Johansson Te2
 
-    public PathProgram(){
-    CreateAndShowGui();
+    public PathProgram() {
+        map = new Map(); //This needs to be before CreateAndShowGui(), since it's using methods from it to make the window
+        CreateAndShowGui();
     }
-    
-public void CreateAndShowGui(){
 
-}
+    public void CreateAndShowGui() {
+        xSize = map.getPathObjectSize() * map.getMapSize();
+        ySize = map.getPathObjectSize() * map.getMapSize();
+        JFrame frame = new JFrame("Shit Pather");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        canvas = new Canvas();
+        canvas.setSize(xSize, ySize);
+        canvas.setVisible(true);
+        frame.setVisible(true);
+        frame.setSize(xSize, ySize);
 
-public void draw(Graphics g){
+        guiPanel.setSize(100,ySize);
 
-}
-public void run(){
+        button = new JButton("Start"); // the button for high attacks
+        button.setVisible(true);
+        button.setText("Start");
+        button.addActionListener(actionHandler);
 
-}
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void run() {
+
+    }
+
+
+
+ private class ActionHandler implements ActionListener//this listens if a action is performed and exceutes the linked action 
+    {
+
+        public void actionPerformed(ActionEvent e) {
+
+            try {
+
+                String cmd = e.getActionCommand();
+                switch (cmd) {
+                    case "start":
+                        
+                        break;
+                  
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            //   }
+        }
+    }
+
 }
