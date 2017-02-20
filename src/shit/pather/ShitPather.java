5d6dd6055203404da5cf72b4233feaf3d6a9be51
ertex <davidlgjohansson@gmail.com>
2017-02-20 09:48:17 +0100

    start

---
+++ src/shit/pather/ShitPather.java
@@ -0,0 +1,12 @@
+package shit.pather;
+
+//Author David Johansson Te2
+//Imports go here!
+public class ShitPather {
+
+    public static void main(String[] args) {
+    System.out.println("ShitPather started");
+//Code goes here!
+    }
+
+}
