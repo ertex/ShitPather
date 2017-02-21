972da0960b1eddd361d83557ac79977b83c57a03
ertex <davidlgjohansson@gmail.com>
2017-02-21 13:58:37 +0100

    tisdagSlut

--- src/shit/pather/Map.java
+++ src/shit/pather/Map.java
@@ -11,7 +11,9 @@ public class Map {
     //  private ArrayList<PathObject> Pathers;
     private int x, y, width, height;
     private final int PathObjectSize = 20;
-    private PathObject OriginPathObject;
+    private boolean foundExit;
+    private byte exitPath[][];
+    private PathObject exitObject;
 
     public Map() {
         mapSize = 20;
@@ -23,7 +25,44 @@ public class Map {
     }
 
     public void updatePathing() {
+        for (byte x = 0; x < mapSize; x++) {
+            for (byte y = 0; y < mapSize; y++) {
+                if (map[x][y].getActive()) {//only updates the active objects
 
+                    if (map[x + 1][y].getType() == 0) {//the following if statements checks to see if there are empty spaces next to it, if so, paste a new object
+                        map[x + 1][y] = new PathObject(x, y, (byte) 1, (byte) (map[x][y].getLenght() + 1));
+                    } else if (map[x + 1][y].getType() == 2) {
+                        foundExit = true;
+                        exitObject = map[x + 1][y];
+                    }
+                    if (map[x - 1][y].getType() == 0) {
+                        map[x - 1][y] = new PathObject(x, y, (byte) 1, (byte) (map[x][y].getLenght() + 1));
+                    } else if (map[x - 1][y].getType() == 2) {
+                        foundExit = true;
+                        exitObject = map[x + 1][y];
+                    }
+                    if (map[x][y + 1].getType() == 0) {
+                        map[x][y + 1] = new PathObject(x, y, (byte) 1, (byte) (map[x][y].getLenght() + 1));
+                    } else if (map[x][y + 1].getType() == 2) {
+                        foundExit = true;
+                        exitObject = map[x][y + 1];
+                    }
+                    if (map[x][y - 1].getType() == 0) {
+                        map[x][y - 1] = new PathObject(x, y, (byte) 1, (byte) (map[x][y].getLenght() + 1));
+                    } else if (map[x][y - 1].getType() == 2) {
+                        foundExit = true;
+                        exitObject = map[x][y - 1];
+                    }
+                    map[x][y].setState(false);
+                }
+            }
+        }
+        if(!foundExit){
+        updatePathing();
+        }
+        else{
+        createExitPath(exitObject);
+        }
     }
 
     public void changeMap(byte x, byte y, byte type) {
@@ -35,29 +74,11 @@ public class Map {
     }
 
     public void checkSurroundings() {
-        for (byte x = 0; x < mapSize; x++) {
-            for (byte y = 0; y < mapSize; y++) {
-                if (map[x][y].getActive()) {//only updates the active objects
-                    if (map[x + 1][y].getType() == 0) {//the following if statements checks to see if there are empty spaces next to it, if so, paste a new object
-                    map[x+1][y] = new PathObject(x,y,1,map[x][y].getLenght()+1);
-                    }
-                    if (map[x - 1][y].getType() == 0) {
 
-                    }
-                    if (map[x][y + 1].getType() == 0) {
-
-                    }
-                    if (map[x][y - 1].getType() == 0) {
-
-                    }
-                    map[x][y].setState(false);
-                }
-            }
-        }
     }
 
-    public void findExit(PathObject origin, PathObject exit) {
-
+    public void createExitPath(PathObject exit) {
+        
     }
 
     public int getPathObjectSize() {
--- src/shit/pather/PathObject.java
+++ src/shit/pather/PathObject.java
@@ -5,44 +5,49 @@ import java.awt.Graphics;
 
 public class PathObject {
 // PathObject and shit.pather belongs to David Johansson Te2
-private int x,y,width,height;
-private boolean foundExit,active;
-private Color color;
-private byte type,lenght;
 
-public PathObject(byte x,byte y,byte type,byte lenght){
-this.x = x;
-this.y = y;
-this.type = type;
-this.lenght = lenght;
+    private int x, y, width, height;
+    private boolean  active;
+    private Color color;
+    private byte type, lenght;
 
-}
+    public PathObject(byte x, byte y, byte type, byte lenght) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.lenght = lenght;
 
-public void CheckSurroundings(){
+    }
 
-}
-public void draw(Graphics g){
+    public void CheckSurroundings() {
 
-}
+    }
 
-public boolean getActive(){
-return active;
-}
+    public void draw(Graphics g) {
 
-public void setState(boolean state){
-active = state;
-}
+    }
 
-public byte getType(){
-return type;
-}
-public int getX(){
-return x;
-}
-public int getY(){
-return y;
-}
-public byte getLenght(){
-return lenght;
-}
+    public boolean getActive() {
+        return active;
+    }
+
+    public void setState(boolean state) {
+        active = state;
+    }
+
+    public byte getType() {
+        return type;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public byte getLenght() {
+        return lenght;
+    }
 }
